bf32a04b0e89b50089fca6e7c23b40bb6073a7c1
Gianmarco Ebeling <gianmarco.ebeling@reda.it>
2022-07-10 19:53:38 +0200

    add reset all filters button

--- frontend/src/components/policies/PoliciesHeader.tsx
+++ frontend/src/components/policies/PoliciesHeader.tsx
@@ -11,12 +11,20 @@ interface IQueryProps {
 }
 
 function PoliciesHeader({ queryData, controller }: IQueryProps) {
+  const resetAllFilters = () => {
+    controller.setSelectedProvider("ALL");
+    controller.setSelectedType("ALL");
+    controller.setSelectedStatus("ALL");
+  };
   return (
     <div>
       <h1 className='text-3xl font-bold text-gray-900 mb-2'>Policies</h1>
       <div className='my-2 flex sm:flex-row flex-col'>
         <PoliciesFilters controller={controller} queryData={queryData} />
         <SearchFilter controller={controller} />
+        <p id='reset' className='flex justify-center items-center ml-4 cursor-pointer' onClick={resetAllFilters}>
+          Reset all
+        </p>
       </div>
     </div>
   );
